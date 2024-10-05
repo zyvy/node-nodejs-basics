99732e7db9d3c8d2a318a8fc3cba07dd7634d33e
zyvy <gm.zyvy@gmail.com>
2024-10-05 23:13:56 +0300

    feat: started tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const copy = async () => {
-    // Write your code here 
+  const srcDir = "files";
+  const destDir = "files_copy";
+  try {
+    await fs.access(srcDir);
+    await fs.access(destDir);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.code !== "ENOENT") throw err;
+    await fs.mkdir(destDir, { recursive: true });
+
+    const files = await fs.readdir(srcDir);
+    for (const file of files) {
+      const srcPath = path.join(srcDir, file);
+      const destPath = path.join(destDir, file);
+      await fs.copyFile(srcPath, destPath);
+    }
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 const create = async () => {
-    // Write your code here 
+  const filename = "fresh.txt";
+  const content = "I am fresh and young";
+  const fullPath = path.join("files", filename);
+  try {
+    const file = await fs.open(fullPath, "wx");
+    await file.writeFile(content);
+    console.log(`File ${filename} created`);
+    await file.close();
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.error("FS operation failed");
+    }
+  }
 };
 
 await create();
