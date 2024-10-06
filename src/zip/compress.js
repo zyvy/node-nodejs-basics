77d2aa0b74eff68a5c36bf3e1119a662c596b0ab
zyvy <gm.zyvy@gmail.com>
2024-10-06 16:42:21 +0300

    feat: zip done

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream("fileToCompress.txt");
+  const writeStream = fs.createWriteStream("archive.gz");
+  const gzip = zlib.createGzip();
+
+  readStream
+    .pipe(gzip)
+    .pipe(writeStream)
+    .on("finish", () => {
+      console.log("Done");
+    })
+    .on("error", (err) => {
+      console.error(err);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import zlib from "zlib";
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream("archive.gz");
+  const writeStream = fs.createWriteStream("fileToCompress.txt");
+  const gunzip = zlib.createGunzip();
+
+  readStream
+    .pipe(gunzip)
+    .pipe(writeStream)
+    .on("finish", () => {
+      console.log("Done");
+    })
+    .on("error", (err) => {
+      console.error(err);
+    });
 };
 
 await decompress();
